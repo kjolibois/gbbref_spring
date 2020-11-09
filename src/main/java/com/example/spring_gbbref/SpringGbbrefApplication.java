c9ebd84a5c26d9cf974e603392132cd8c5c723c4
KhalilJ <khaliljolibois@gmail.com>
2020-11-08 21:56:48 -0500

    first commit

---
+++ src/main/java/com/example/spring_gbbref/SpringGbbrefApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_gbbref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGbbrefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGbbrefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_gbbref/SpringGbbrefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_gbbref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGbbrefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
